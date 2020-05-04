07dd3b5f5cea411a7da72e24683d07cc01e659de
sdas00126 <64783518+sdas00126@users.noreply.github.com>
2020-05-04 17:23:22 +0530

    Add files via upload

---
+++ task0.m
@@ -0,0 +1,36 @@
+clc;
+clear all;
+close all;
+disp('Welcome to BMI Calculator');
+s = input('Enter your name');
+choice = menu('Welcome to BMI calculator','Imperial System','SI standard system');
+if(choice==1)
+   w=input('Enter your weight in pounds');
+     h=input('Enter your height in inches');
+       bmi=703*w/(h*h);
+elseif(choice==2)
+    w=input('Enter your weight in kg');
+      h=input('Enter your height in metre');
+       bmi=w/(h*h);
+else
+    bmi=0;
+end
+if(bmi>0 && bmi<=18.49)
+    c='Underweight';
+elseif(bmi>=18.5 && bmi<25)
+    c='Healthy';
+elseif(bmi>=25 && bmi<30)
+    c='Overweight'
+elseif(bmi>=30)
+    c='Obese'
+end
+if(bmi>0)
+    disp('Name:');
+    disp(s);
+    disp('Body Mass Index(BMI)=');
+    disp(bmi);
+    disp('Category of BMI=');
+    disp(c);
+else
+    disp('Unfortunately you closed the menu TRY AGAIN')
+ end
